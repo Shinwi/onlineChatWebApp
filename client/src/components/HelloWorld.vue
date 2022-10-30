a35843ba45b9e1b7fd60b8e56786d1e96699b915
Oualid Ezzoubeidi <siszko37@gmail.com>
2022-10-30 16:25:45 +0100

    setting up the project structure

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h4>comp</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
