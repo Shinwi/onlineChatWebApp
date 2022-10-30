4f7dfb933af95820cf3bf4ba2a4403814f090f7b
Oualid Ezzoubeidi <siszko37@gmail.com>
2022-10-30 18:28:23 +0100

    minimal homepage added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,22 +1,75 @@
 <template>
-  <h2>hello</h2>
+  <div id="app">
+    <div v-if="isChatRoom">
+      <chatRoom :socket="socket" :userName="userName"/>
+    </div>
+    <div v-else>
+      <form class="form">
+        <h1>Karta</h1>
+        <div>
+          <input type="text" v-model="userName" placeholder="Plese enter your name">
+        </div>
+        <div>
+          <input type="text" v-model="inviteRoomCode" placeholder="Join a game">
+          <button class="btn" :disabled="!inviteRoomCode.length || !userName.length" @click="joinRoom()">Join</button>
+        </div>
+        <button class="btn" :disabled="!userName.length"  @click="createRoom()">Create room</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-
-
+import io from "socket.io-client";
+import chatRoom from "./components/ChatRoom.vue"
 export default {
   name: 'App',
+  data() {
+    return {
+      socket: {},
+      userName: '',
+      inviteRoomCode: '',
+      isChatRoom: false
+    }
+  },
+  components: {
+    chatRoom
+  },
+  created () {
+    // connecting to the server
+    this.socket = io('http://localhost:3000')
+
+    // connect and create get user socket id : roomCode
+    this.socket.on('connect', () => {
+      console.log('user id connected: ' + this.socket.id)
+    })
+  },
+  methods: {
+    createRoom () {
+      if (!this.userName.length) { 
+        alert('User name cannot be empty')
+        return
+      }
+      // alert('creating room: ' + this.playerName)
+      this.socket.emit('createRoom', {userName: this.userName})
+      this.isChatRoom = true
+    },
+    joinRoom () {
+      if (!this.inviteRoomCode.length || !this.userName.length) {
+        alert('Username and room code cannot be empty')
+        return
+      }
+      // alert('joining room: ' + this.inviteRoomCode)
+      this.socket.emit('joinRoom', {userName: this.userName, roomCode: this.inviteRoomCode})
+      this.isChatRoom = true
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
 }
 </style>
